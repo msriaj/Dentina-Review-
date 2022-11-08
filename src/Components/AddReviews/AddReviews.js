60f4d8df4d347a657291141ada281190b9ab088d
md-riaj11 <contact.riaj@gmail.com>
2022-11-09 03:48:11 +0600

    write a review form added

---
+++ src/Components/AddReviews/AddReviews.js
@@ -0,0 +1,78 @@
+import React, { useContext } from "react";
+import { FaNotesMedical } from "react-icons/fa";
+import { AuthContext } from "../../Context/AuthContext";
+
+const AddReviews = () => {
+  const { user } = useContext(AuthContext);
+  return (
+    <>
+      <h1 className="text-xl font-medium title-font mb-1 text-gray-600 flex items-center">
+        <FaNotesMedical /> Write A Review
+      </h1>
+      <div className="bg-gray-200 rounded-lg shadow">
+        <section className="text-gray-600 body-font relative">
+          <div className="container p-6  mx-auto">
+            <div className="mx-auto">
+              <div className="flex flex-wrap -m-2">
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label
+                      for="name"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="w-full bg-white bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                </div>
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label
+                      for="email"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="w-full bg-white bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                </div>
+                <div className="p-2 w-full">
+                  <div className="relative">
+                    <label
+                      for="message"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      className="w-full bg-white bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="p-2 w-full">
+                  <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                    Button
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default AddReviews;
--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import AddReviews from "../../Components/AddReviews/AddReviews";
+import Reviews from "../../Components/Reviews/Reviews";
 
 const ServiceDetails = () => {
   const { _id, title, descriptions, thumbURL, price } = useLoaderData();
   return (
     <div className="bg-blue-50 py-12">
       <div className="md:w-10/12 mx-auto bg-white rounded-md p-6 flex">
-        <div className="w-4/12">left</div>
-        <div className="w-8/12">
+        <div className="w-3/12"></div>
+        <div className="w-9/12">
           <img src={thumbURL} alt="" className="w-full" />
           <div className="border-b my-3">
             <h1 className="text-2xl text-gray-800 font-bold">
@@ -19,6 +21,8 @@ const ServiceDetails = () => {
               <b>Descriptions :</b> {descriptions}
             </p>
           </div>
+          <Reviews serviceId={_id} />
+          <AddReviews serviceId={_id} />
         </div>
       </div>
     </div>
